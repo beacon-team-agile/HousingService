9d87fab61131460ede0216a9f7a3e6448f962e14
EigentlicherVogel <peregrinanatum@outlook.com>
2022-08-19 04:26:45 -0400

    abstract objects full

---
+++ src/main/java/com/teamagile/housingservice/domain/LandlordAbstract.java
@@ -0,0 +1,30 @@
+package com.teamagile.housingservice.domain;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.*;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class LandlordAbstract implements Serializable {
+
+    private Integer Id;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String email;
+
+    private String cellPhone;
+
+}
